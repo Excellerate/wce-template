4005533385de2cdb2399a215dad322e83bb3b5d0
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2016-09-06 05:48:20 +0200

    Various improvements

--- parts/content.php
+++ parts/content.php
@@ -1,5 +1,5 @@
 <div id="main" class="ui main container grid">
-	<div class="ui column">
+	<div class="ui centered twelve wide column">
 		<!--<jdoc:include type="message" />-->
 		<jdoc:include type="component" />
 	</div>
